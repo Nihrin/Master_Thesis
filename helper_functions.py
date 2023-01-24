024ddc1d4c0f7eb47c070b3ff906100bc40e73b0
Nihrin <rob.janssen1998@live.nl>
2023-01-24 16:21:06 +0100

    Add data set and start with automating results

--- helper_functions.py
+++ helper_functions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import math
 import random
-import numpy as np
+import pickle
 
 
 def create_missing_data(data: pd.DataFrame, percentage):
@@ -13,41 +13,55 @@ def create_missing_data(data: pd.DataFrame, percentage):
         row = random.randint(0, row_length - 1)
         col = random.randint(0, col_length - 1)
         cells.add((row, col))
-
+    
     for cell in cells:
-        data.iat[cell[1], cell[0]] = np.nan
+        data.iat[cell[1], cell[0]] = None
 
     return data
 
 
-def discounted_accuracies(y, NBC_predictions, NCC_predictions):
-    NBC_acc = (sum(1 for a, b in zip(NBC_predictions, y)
-               if a == b) / len(y)) * 100
-    print('NBC accuracy: ' + str("%.2f" % NBC_acc) + '%')
+def accuracies(y, classical_predictions, credal_predictions):
+    classical_accuracy = (sum(1 for a, b in zip(classical_predictions, y)
+                              if a == b) / len(y)) * 100
+    # print('Classical accuracy: ' + str("%.2f" % classical_accuracy) + '%')
 
-    NCC_acc = (sum(1 for a, b in zip(NCC_predictions, y)
-               if a == b) / len(y)) * 100
-    print('NCC accuracy: ' + str("%.2f" % NCC_acc) + '%')
+    credal_accuracy = (sum(1 for a, b in zip(credal_predictions, y)
+                           if a == b) / len(y)) * 100
+    # print('Credal accuracy: ' + str("%.2f" % credal_accuracy) + '%')
 
-    correct = 0
-    for a, b in zip(NCC_predictions, y):
+    d_correct = 0
+    r_correct = 0
+    r_count = 0
+    for a, b in zip(credal_predictions, y):
         if a == b:
-            correct += 1
+            d_correct += 1
+            r_correct += 1
+            r_count += 1
         elif b in a:
-            correct += 1 / len(a)
-    NCC_d_acc = (correct / len(y)) * 100
-    print('NCC discounted accuracy: ' + str("%.2f" % NCC_d_acc) + '%')
+            d_correct += 1 / len(a)
+        elif type(a) != list():
+            r_count += 1
+    discounted_credal_accuracy = (d_correct / len(y)) * 100
+    robust_credal_accuracy = (r_correct / r_count) * 100
 
-    return NBC_acc, NCC_acc, NCC_d_acc
+    # print('Discounted credal accuracy: ' + str("%.2f" %
+    #       discounted_credal_accuracy) + '%')
+    # print('Robust credal accuracy: ' + str("%.2f" %
+    #       robust_credal_accuracy) + '%')
+
+    return classical_accuracy, credal_accuracy, discounted_credal_accuracy, robust_credal_accuracy
 
 
-def exact_accuracies(y, NBC_predictions, NCC_predictions):
-    NBC_e_acc = (sum(1 for a, b in zip(
-        NBC_predictions, y) if a == b) / len(y)) * 100
-    print('NBC robust accuracy: ' + str("%.2f" % NBC_e_acc) + '%')
+def create_names_dict():
+    dict = {
+        'balance-scale': ['classes', 'left-weight', 'left-distance', 'right-weight', 'right-distance'],
+        'iris': ['sepal_l', 'sepal_w', 'petal_l', 'petal_w', 'classes']      
+    }
+    with open('UCI_data_names.pkl', 'wb') as f:
+        pickle.dump(dict, f)
 
-    NCC_e_acc = (sum(1 for a, b in zip(
-        NCC_predictions, y) if a == b) / len(y)) * 100
-    print('NCC robust accuracy: ' + str("%.2f" % NCC_e_acc) + '%')
 
-    return NBC_e_acc, NCC_e_acc
+def open_names_dict():
+    with open('UCI_data_names.pkl', 'rb') as f:
+        dict = pickle.load(f)
+    return dict
--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import helper_functions
 import math
 
 
-def run(missing):
-    names = ['sepal_l', 'sepal_w', 'petal_l', 'petal_w', 'classes']
-    data = pd.read_csv('data/iris.data', names=names)
-    y = data['classes']
-    X = data.drop(['classes'], axis=1)
-
+def run_naive_classifiers(X, y, missing):
     X = helper_functions.create_missing_data(X, missing)
     X = X.fillna(X.mean())
 
@@ -21,48 +16,52 @@ def run(missing):
     NCC.train(X, y)
     NCC_predictions = NCC.predict(X)
 
-    NBC_acc, NCC_acc, NCC_d_acc = helper_functions.discounted_accuracies(
+    NBC_acc, NCC_acc, NCC_d_acc, NCC_r_acc = helper_functions.accuracies(
         y, NBC_predictions, NCC_predictions)
 
-    # print('Mistakes:')
-    # print('Index \t Prediction \t \t True')
-
-    indices = []
-    for i in range(len(NCC_predictions)):
-        # if NCC_predictions[i] != y[i]:
-        #     print(i+1, '\t', NCC_predictions[i], '\t', y[i])
-        if type(NCC_predictions[i]) == list:
-            indices.append(i)
-
-    y_ = y.copy().to_list()
-
-    for i in sorted(indices, reverse=True):
-        del NCC_predictions[i]
-        del NBC_predictions[i]
-        del y_[i]
-
-    NBC_e_acc, NCC_e_acc = helper_functions.exact_accuracies(
-        y_, NCC_predictions, NBC_predictions)
-
-    return NBC_acc, NCC_acc, NCC_d_acc, NBC_e_acc, NCC_e_acc
+    return NBC_acc, NCC_acc, NCC_d_acc, NCC_r_acc
 
 
-runs = 10
-missing = 50
-NBC_acc_, NCC_acc_, NCC_d_acc_, NBC_e_acc_, NCC_e_acc_ = list(
-), list(), list(), list(), list()
-for a in range(runs):
-    NBC_acc, NCC_acc, NCC_d_acc, NBC_e_acc, NCC_e_acc = run(missing)
-    NBC_acc_.append(NBC_acc)
-    NCC_acc_.append(NCC_acc)
-    NCC_d_acc_.append(NCC_d_acc)
-    NBC_e_acc_.append(NBC_e_acc)
-    NCC_e_acc_.append(NCC_e_acc)
+def naive_classifiers(X, y, missing: int = 0, runs: int = 10):
+    NBC_acc_list, NCC_acc_list, NCC_d_acc_list, NCC_r_acc_list = list(
+    ), list(), list(), list()
+
+    for _ in range(runs):
+        NBC_acc, NCC_acc, NCC_d_acc, NCC_r_acc = run_naive_classifiers(
+            X.copy(), y, missing)
+        NBC_acc_list.append(NBC_acc)
+        NCC_acc_list.append(NCC_acc)
+        NCC_d_acc_list.append(NCC_d_acc)
+        NCC_r_acc_list.append(NCC_r_acc)
+
+    print("Overview:")
+    print("NBC_acc: " + str("%.2f" % (math.fsum(NBC_acc_list) / runs)) + '%')
+    print("NCC_acc: " + str("%.2f" % (math.fsum(NCC_acc_list) / runs)) + '%')
+    print("NCC_d_acc: " + str("%.2f" % (math.fsum(NCC_d_acc_list) / runs)) + '%')
+    print("NCC_r_acc: " + str("%.2f" % (math.fsum(NCC_r_acc_list) / runs)) + '%')
+
+
+def run():
+    iris_names = ['sepal_l', 'sepal_w', 'petal_l', 'petal_w', 'classes']
+    iris_data = pd.read_csv('UCI_data/iris.data', names=iris_names)
+    iris_y = iris_data['classes']
+    iris_X = iris_data.drop(['classes'], axis=1)
+
+    balance_names = ['classes', 'left-weight', 'left-distance', 'right-weight', 'right-distance']
+    balance_data = pd.read_csv('UCI_data/balance-scale.data', names=balance_names)
+    balance_y = balance_data['classes']
+    balance_X = balance_data.drop(['classes'], axis=1)
+
+    missing_data = 30
+    cross_validations = 10
+
+    print('Iris')
+    naive_classifiers(iris_X, iris_y, missing_data, cross_validations)
     print()
+    print('Balance')
+    naive_classifiers(balance_X, balance_y, missing_data, cross_validations)
 
-print("Overview:")
-print("NBC_acc: " + str("%.2f" % (math.fsum(NBC_acc_) / runs)) + '%')
-print("NCC_acc: " + str("%.2f" % (math.fsum(NCC_acc_) / runs)) + '%')
-print("NCC_d_acc: " + str("%.2f" % (math.fsum(NCC_d_acc_) / runs)) + '%')
-print("NBC_e_acc: " + str("%.2f" % (math.fsum(NBC_e_acc_) / runs)) + '%')
-print("NCC_e_acc: " + str("%.2f" % (math.fsum(NCC_e_acc_) / runs)) + '%')
+# helper_functions.create_names_dict()
+# names_dict = helper_functions.open_names_dict()
+# print(names_dict)
+run()
